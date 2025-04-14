43b171dbdf4ecebe89a40b0d4076b21aa36d4fc9
harmeshp <harmesh@mpcloud.com>
2025-04-14 15:56:20 +0530

    remove temp app

--- CultureTestApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Globalization;
-
-Console.WriteLine("Hello, World!");
-
-
-
-Console.WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.Name}");
-Console.WriteLine($"Invariant? {AppContext.GetData("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT")}");
